5e3708a782f60b5fb28d1b911ec45f41626ab8b6
ARBUCHELI <radio_seattle@hotmail.com>
2024-12-18 17:03:40 +0300

    learn logic gates - not gate

---
+++ LEARN LOGIC GATES/not-gate/script.py
@@ -0,0 +1,11 @@
+#from nand import NAND_gate
+
+def NOT_gate(a):
+  if a:
+    return 0
+  else:
+    return 1
+
+# TEST CASE
+print("A: 0 | Output: {0}".format(NOT_gate(0)))
+print("A: 1 | Output: {0}".format(NOT_gate(1)))
